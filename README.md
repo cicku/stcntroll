4c0bdf724a777bc9e0f1344f8056f2331cf3b03f
Christopher Meng <i@cicku.me>
2015-01-15 20:46:57 +0800

    Embryo
    Initial version committed, preliminary.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # stcntroll
-This is a "handy" but enigmatic tool to pick up the lucky dog \^_^/
+A simple tool written in OCaml to generate a random number to be used in game roll at steamcn.
+
+It's just a embryo, further development:
+
+1. Use function instead;
+2. Exclude zero;
+
+**Have fun!**
---
+++ stcntroll.ml
@@ -0,0 +1,24 @@
+(*
+ * Copyright (c) 2015 Christopher Meng.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by the <organization>. The name of the
+ * <organization> may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * *)
+
+(* LOL we need real numbers. *)
+Random.self_init ();;
+
+(* Roll for one lucky dog ONLY! *)
+for i = 0 to 0 do
+        print_int (Random.int 199); print_string "\n"
+done;;
